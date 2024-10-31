ea6c2cd2d60178aae74eb05c87940a2f9ef3edd9
Tristan H <tristanskyhook@gmail.com>
2024-10-31 08:30:27 -0700

    new data & title change

--- src/components/title/title.jsx
+++ src/components/title/title.jsx
@@ -6,7 +6,7 @@ export default function Title( {lastUpdated, showLastUpdated} ) {
             <h1>Cross-Species</h1>
             <h1>H5N1 US Tracker</h1>
             <div className={styles.divider}></div>
-            <h3 className={'fontS'}>{'Data represents public feeds from\nUSDA APHIS, US CDC, and state\ndepartments of health in\nColorado, Michigan and Texas.'}</h3>
+            <h3 className={'fontS'}>{'Data represents public feeds from USDA APHIS, US CDC,\nand state departments of health in California,\nColorado, Michigan, Missouri, Texas, and Washington.'}</h3>
             {showLastUpdated && <h5 className={'fontS'}>{'Last updated: ' + lastUpdated}</h5>}
         </div>
     );
